5048d198820783659b0c83f1a56796c573dcc6df
owais34 <owaishunter78@gmail.com>
2020-09-21 13:41:32 +0530

    Updates

---
+++ precache-manifest.034a41ab73e5e5d187c9e747847f306b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0400f9f0b8cb965c29c5d24f764dc3",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "adf060cd352067272eb9",
+    "url": "/weather-app-react/static/css/main.d0162149.chunk.css"
+  },
+  {
+    "revision": "fa4421bae2d960bbb887",
+    "url": "/weather-app-react/static/js/2.71c35cf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-react/static/js/2.71c35cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf060cd352067272eb9",
+    "url": "/weather-app-react/static/js/main.75ebc6ce.chunk.js"
+  },
+  {
+    "revision": "0965184a592c71c34fe2",
+    "url": "/weather-app-react/static/js/runtime-main.0d456eba.js"
+  },
+  {
+    "revision": "97e016c668fee51887ef4fdaf8dba16c",
+    "url": "/weather-app-react/static/media/stormy.97e016c6.webp"
+  }
+]);
